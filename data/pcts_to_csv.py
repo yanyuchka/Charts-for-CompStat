5ae40cf35f5e984314ebdd72afa83df3a8223a26
yanyuchka <yanyuchka@gmail.com>
2015-05-03 14:03:59 -0400

    fixed pcts csv files

--- data/pcts_to_csv.py
+++ data/pcts_to_csv.py
@@ -6,10 +6,21 @@ dpath = './'
 
 crash = pd.read_csv(dpath+"crash_precincts.csv")
 
+for year in crash.year.unique():
+  crash['year'].loc[(crash['week']==1) & (crash['year']==year) & (crash['label'].str[0:4] == str(year))
+] = crash['year'].loc[(crash['week']==1) & (crash['year']==year) & (crash['label'].str[0:4] == str(year))].apply(lambda x: x+1)
+
+crash = crash.groupby(['year', 'week','precinct','label'])['all_collisions','injury_collisions','fatal_collisions','injures','fatalities','cyclists_involved','pedestrians_involved'].sum().reset_index()
+
+#stops = stops.groupby(['year','week','precinct'])['stops'].sum().reset_index()
+
 pcts = sorted(crash.precinct.unique())
 
 for pct in pcts:
-  pct_csv = crash[crash['precinct']==pct]
-  pct_csv.to_csv(dpath+"../page/csv/pcts/collisions_%s.csv" % (pct), index = False)
-  print '<option value="%s">Precinct %s</option>' % (pct,pct)
+  pct_csv = crash[crash['precinct']==int(pct)].sort(['year','week'])
+  pct_csv['index'] = range(1, len(pct_csv) + 1)
+  #pct_csv['year'].loc[(pct_csv['week']==1) & (crash['year']==2012) & pct_csv['label'][]] = crash['year'].loc[(crash['week']==1) & (crash['year']==2013)].apply(lambda x: x+1)
+
+  pct_csv.to_csv(dpath+"../page/csv/pcts/collisions_%s.csv" % (pct), index=False)
+  #print '<option value="%s">Precinct %s</option>' % (pct,pct)
 
