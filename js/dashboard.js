8f151333edd532a7014f99f307eeae48c1697fd1
Neil P. Ramos <neil.p.ramos@gmail.com>
2015-05-08 17:56:01 -0400

    changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,7 +6,8 @@
 var thisFileName = "dashboard.js";
 var list_of_precincts = [1,5,6,7,9,10,13,14,17,18,19,20,22,23,24,25,26,28,30,32,33,34,40,41,42,43,44,45,46,47,48,49,50,52,60,61,62,63,66,67,68,69,70,71,72,73,75,76,77,78,79,81,83,84,88,90,94,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,120,121,122,123];
 var initialPrecinct = 1;
-var selectedIndex = 100;
+var selectedIndex = null;
+var selectedDate = null;
 var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 // --- Sparkline Global Variables
@@ -25,6 +26,7 @@ var sparkline = {
   csvFileDirectory: "./csv/pcts/",
   csvFileName: "collisions_",
   csvFileExtension: ".csv",
+  dateArray: null,
 };
 
 
@@ -72,7 +74,7 @@ var dailytrendline = {
   dataset: null,
   redraw: null,
   loadCSV: null,
-  csvFileDirectory: "./csv/pcts_daily/",
+  csvFileDirectory: "./csv/pcts/",
   csvFileName: "collisions_",
   csvFileExtension: ".csv",
   width: 0,
@@ -91,6 +93,27 @@ dailytrendline.height = 100 - dailytrendline.top - dailytrendline.bottom;
 
 
 
+
+
+// --- Daily trend line
+var largetrendline = {
+  dataset: null,
+  redraw: null,
+  loadCSV: null,
+  csvFileDirectory: "./csv/pcts/",
+  csvFileName: "collisions_",
+  csvFileExtension: ".csv",
+  width: 0,
+  height: 0,
+  left: 30,
+  right: 10,
+  top: 10,
+  bottom: 10,
+}
+
+
+largetrendline.width = 950 - largetrendline.left - largetrendline.right,
+largetrendline.height = 100 - largetrendline.top - largetrendline.bottom;
 
 
 
@@ -139,7 +162,7 @@ function initDashboard()
   sparkline.loadCSV(sparkline.csvFileDirectory + sparkline.csvFileName + initialPrecinct + sparkline.csvFileExtension);
   
   // Load the Daily Trend Line Dataset
-  dailytrendline.loadCSV(dailytrendline.csvFileDirectory + dailytrendline.csvFileName + initialPrecinct + dailytrendline.csvFileExtension);
+  // dailytrendline.loadCSV(dailytrendline.csvFileDirectory + dailytrendline.csvFileName + initialPrecinct + dailytrendline.csvFileExtension);
 }
 
 
@@ -185,15 +208,37 @@ function initDropdownDates()
 {  
   // Append each of the dates to the dropdown
   sparkline.dataset.forEach(function(d){$( "<option value=\"" + d.index + "\">" + d.label + "</option>" ).appendTo( $( "#select_dates" ) );});
+  
+  // Set the initial selectedIndex
+  selectedIndex = d3.max(sparkline.dataset, function(d){return d.index;});
+
+  // Set the initial selectedDate
+  selectedDate = d3.max(sparkline.dataset, function(d){return d.date;});
+
+  // Set the current date range to the most recent date
+  $( "#select_dates").val(d3.max(sparkline.dataset, function(d){return d.index;}));
+
+
 
   // On-click
   $( "#select_dates" ).change(function(){
     selectedIndex = $("#select_dates").val();
+
+    // Extract the first and second date from the label
+    var first_date = sparkline.dataset[selectedIndex].label.slice(0,10);
+    var second_date = sparkline.dataset[selectedIndex].label.slice(19,30);
+
+    // Display the report dates at the top label
+    $( "#date-range" ).val(first_date + " - " + second_date);
+    selectedDate = parseDate(second_date);
+
+
     // TODO: make this show the actual date being loaded, not the index number
     log("Loading Date: ", selectedIndex);
 
     // Redraw all sparklines
     sparkline.redraw();
+    largetrendline.redraw();
   });
 
   log("Initialize Dropdown Dates", "initDropdownDates");
@@ -222,6 +267,7 @@ sparkline.loadCSV = function(filename)
   log("Loading Sparkline CSV.", "sparkline.loadCSV" + ": " + filename);
   // Clear the dataset and date array
   sparkline.dataset = [];
+  sparkline.dateArray = [];
 
   
   // Load daily trendline dataset
@@ -243,15 +289,19 @@ sparkline.loadCSV = function(filename)
             row_number: +d.row_number,
             label: d.label,
             index: +d.index,
+            date: parseDate(d.label.slice(19,30)),
           })
-    
-    }); //data.forEach
 
-    // Initial draw
-    sparkline.redraw();
+        // Add the week ending date to the dateArray
+        sparkline.dateArray.push(parseDate(d.label.slice(19,30)));
+    }); //data.forEach
 
     // Populate the dropdown with dates
     initDropdownDates();
+
+    // Initial draw
+    sparkline.redraw();
+    largetrendline.redraw();
 
     log("Done Loading.", "sparkline.loadCSV" + ": " + filename);
    
@@ -284,6 +334,10 @@ sparkline.redraw = function()
 
 
 
+
+
+
+
 /*  Draw a single sparkline
  *  @param:  string    id of the div
  *  @param:  string    csv attribute of the sparkline to draw
@@ -291,63 +345,41 @@ sparkline.redraw = function()
 */
 sparkline.draw = function(id, attribute)
 {
-  log("Drawing: " + attribute, "sparkline.draw");
-
-  // Domain with y inverted
-  var x = d3.scale.linear().range([0, sparkline.width - sparkline.top]);
-  var y = d3.scale.linear().range([sparkline.height - sparkline.left, 0]);
-
-  x.domain([0,selectedIndex]);
-  y.domain([0, d3.max(sparkline.dataset, function(d) { return d[attribute]; })]);
-
-
-  // Create the line 
+  var x = d3.scale.linear().range([0, sparkline.width - 2]);
+  var y = d3.scale.linear().range([sparkline.height - 4, 0]);
+  var parseDate = d3.time.format("%Y-%m-%d").parse;
   var line = d3.svg.line()
-      .interpolate("linear")
-      // .interpolate("basis")
-      .x(function(d) { return x(d.index); })
-      .y(function(d) { return y(d[attribute]); });
+               .interpolate("basis")
+               .x(function(d) { return x(d.date); })
+               .y(function(d) { return y(d[attribute]); });
 
-  // Set the color of the trend line based on start and end 
-  dataMin = d3.min(sparkline.dataset, function(d) { return d[attribute]; });
-  dataMax = d3.max(sparkline.dataset, function(d) { return d[attribute]; });
 
-  //TODO: Ask Alex if he wants the sparkline color to change
-  // var trendcolor = (sparkline.dataset[dataMin][attribute] >= sparkline.dataset[dataMax][attribute] ? "sparkline color_decreasing" : "sparkline color_increasing");
-  // log(sparkline.dataset[dateslider.indexLow][attribute] + " >= " + sparkline.dataset[dateslider.indexHigh][attribute], "sparkline.draw");  
+    // x.domain(d3.extent(sparkline.dataset, function(d){return d.date;}));
+    x.domain([d3.min(sparkline.dataset, function(d){return d.date;}), selectedDate]);
+    y.domain(d3.extent(sparkline.dataset, function(d) { return d[attribute]; }));
 
-  trendcolor = "sparkline color_black";
 
   // Remove the existing svg then draw
   d3.select(id).select("svg").remove();
 
-
-
   // Redraw the svg
   var svg = d3.select(id).append("svg")
-        .attr("width", sparkline.width + sparkline.left + sparkline.right)
-        .attr("height", sparkline.height + sparkline.top + sparkline.bottom)
-        .append("g")
-        .attr("transform", "translate(" + sparkline.left + "," + sparkline.top + ")");
+       .attr('width', sparkline.width)
+                .attr('height', sparkline.height)
+                .append('g')
+                .attr('transform', 'translate(0, 2)');
 
-
-  // Draw the trend line
-  svg.append("path")
-      .datum(sparkline.dataset)
-      .attr("class", trendcolor)
-      .attr("d", line);
-
-  // svg.append('circle')
-  //    .attr('class', 'sparkcircle')
-  //    .attr('cx', x(selectedIndex))
-  //    .attr('cy', y( sparkline.dataset[146][attribute] ))
-  //    .attr('r', 2.5);
+    svg.append('path')
+       .datum(sparkline.dataset)
+       .attr('class', 'sparkline')
+       .attr('d', line);
+    svg.append('circle')
+       .attr('class', 'sparkcircle')
+       .attr('cx', x(selectedDate))
+       .attr('cy', y(sparkline.dataset[selectedIndex][attribute]))
+       .attr('r', 1.5);  
+       console.log(selectedIndex);
 }
-
-
-
-
-
 
 
 
@@ -369,7 +401,7 @@ sparkline.draw = function(id, attribute)
 */
 dailytrendline.loadCSV = function(filename)
 {
-  log("Loading Dailytrendline CSV.", "sparkline.loadCSV" + ": " + filename);
+  log("Loading Dailytrendline CSV.", "dailytrendline.loadCSV" + ": " + filename);
   // Clear the dataset and date array
   dailytrendline.dataset = [];
   dateslider.dateArray = [];
@@ -509,6 +541,113 @@ dailytrendline.redraw = function()
 }
 
 
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------//
+//                                   LARGE TREND LINE
+//---------------------------------------------------------------------------------//
+
+
+
+/*  Draw the large trend line
+ *  @param:  string    id of the div
+ *  @return: None
+*/
+largetrendline.draw = function(id, attribute)
+{
+  log("Drawing: " + attribute, "largetrendline.draw");
+
+  // Scales and axes. Note the inverted domain for the y-scale: bigger is up!
+  var x = d3.time.scale()
+            .domain(d3.extent(sparkline.dataset, function(d) { return d.date;}))
+            .range([0, largetrendline.width+largetrendline.right+largetrendline.left]);
+      
+      y = d3.scale.linear()
+            .domain([0, d3.max(sparkline.dataset, function(d) { return d[attribute]; })])
+            .range([largetrendline.height, 0]);
+
+      xAxis = d3.svg.axis()
+        .scale(x)
+        .tickSize(-largetrendline.height)
+        .tickSubdivide(false)
+        .orient("bottom");
+
+      yAxis = d3.svg.axis()
+        .scale(y)
+        .ticks(3)
+        .orient("left");
+
+
+  // Create the line 
+  var line = d3.svg.line()
+      .interpolate("linear")
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d[attribute]); });
+
+
+
+  // An area generator, for the light fill.
+var area = d3.svg.area()
+    .interpolate("monotone")
+    .x(function(d) { return x(d.date); })
+    .y0(dailytrendline.height)
+    .y1(function(d) { return y(d[attribute]); });
+
+
+  // var trendcolor = "large_trend_line area_increasing";
+  var trendcolor = "large_trend_line color_black";
+  
+  // Remove the existing svg then draw
+  d3.select(id).select("svg").remove();
+
+  // Redraw the svg
+  var svg = d3.select(id).append("svg")
+        .attr("width", dailytrendline.width + dailytrendline.left+dailytrendline.left + 1 + dailytrendline.right)
+        .attr("height", dailytrendline.height + (dailytrendline.top*2) + dailytrendline.bottom)
+        .append("g")
+        .attr("transform", "translate(" + dailytrendline.left + "," + dailytrendline.top + ")");
+        
+
+
+  // Draw the trend line
+  svg.append("path")
+      .datum(sparkline.dataset)
+      .attr("class", trendcolor)
+      .attr("d", area);
+
+      // Add the x-axis.
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + dailytrendline.height + ")")
+      .call(xAxis);
+
+  // Add the y-axis.
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(0,0)")
+      .call(yAxis);
+}
+
+
+
+
+
+
+/*  Redraw all of the daily trend lines
+ *  @param:  None
+ *  @return: None
+*/
+largetrendline.redraw = function()
+{
+  largetrendline.draw("#dailytrend1","all_collisions");
+  largetrendline.draw("#dailytrend2","injury_collisions");
+  largetrendline.draw("#dailytrend3","fatal_collisions");
+}
 
 
 
